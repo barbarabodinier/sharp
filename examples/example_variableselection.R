c93845f3e98ff0bea39a809cb843e2f46a79c03a
barbarabodinier <b.bodinier@imperial.ac.uk>
2021-04-29 20:23:20 +0200

    remove folder examples

--- examples/example_variableselection.R
+++
@@ -1,3 +0,0 @@
-# Variable selection in linear regression
-simul=SimulateRegression(n=100, pk=20, family="gaussian") # simulated data
-out=VariableSelection(xdata=simul$X, ydata=simul$Y, family="gaussian")
