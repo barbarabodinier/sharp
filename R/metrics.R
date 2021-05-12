12021eba720ed711e863c94e9022632aa15d9cda
barbarabodinier <b.bodinier@imperial.ac.uk>
2021-05-12 19:58:00 +0200

    remove block matrix in variable selection

--- R/metrics.R
+++ R/metrics.R
@@ -96,7 +96,7 @@ StabilityMetrics <- function(selprop, pk = NULL, pi_list = seq(0.6, 0.9, by = 0.
 
   # Create matrix with block indices
   nblocks <- 1
-  if (graph) {
+  if (graph) { # to avoid memory issues in high dimensional variable selection
     bigblocks <- BlockMatrix(pk)
     bigblocks_vect <- bigblocks[upper.tri(bigblocks)]
     N_blocks <- unname(table(bigblocks_vect))
