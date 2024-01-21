5b53f298d229adafcfdfa2d8fc36acafcac0e0ce
Barbara Bodinier <barbara.bodinier@gmail.com>
2024-01-21 20:11:36 +0100

    test nloptr

---
+++ tests/testthat/test-nloptr.R
@@ -0,0 +1,23 @@
+test_that("The optimisation with nloptr is working for VariableSelection().", {
+  skip_on_cran()
+  set.seed(1)
+  simul <- SimulateRegression(n = 100, pk = 50, family = "gaussian")
+  stab_grid <- VariableSelection(
+    xdata = simul$xdata,
+    ydata = simul$ydata,
+    family = "gaussian",
+    K = 10,
+    optimisation = "grid_search",
+    verbose = FALSE
+  )
+  stab_nloptr <- VariableSelection(
+    xdata = simul$xdata,
+    ydata = simul$ydata,
+    family = "gaussian",
+    K = 10,
+    optimisation = "nloptr",
+    verbose = FALSE
+  )
+  expect_lt(max(stab_grid$S, na.rm=TRUE), max(stab_nloptr$S, na.rm=TRUE))
+  expect_lt(length(stab_nloptr$Lambda), 100)
+})
